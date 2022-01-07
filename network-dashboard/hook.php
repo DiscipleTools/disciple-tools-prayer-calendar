4ba2361dfc7bef041149186fafe962f017bcc8b3
Chris Chasm <chris@chasm.solutions>
2022-01-07 11:06:02 -0700

    Added network dashboard post section

--- network-dashboard/hook.php
+++ network-dashboard/hook.php
@@ -3,6 +3,12 @@ if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
 
 add_action( 'dt_insert_report', function($args){
 
+    if ( ! function_exists( 'dt_network_site_id' ) ) {
+        return;
+    }
+    if ( ! class_exists( 'DT_Network_Dashboard_Metrics_Base') ) {
+        return;
+    }
     if ( ! isset( $args['type'], $args['subtype'], $args['post_id'] ) ) {
         return;
     }
@@ -34,15 +40,17 @@ add_action( 'dt_insert_report', function($args){
 add_filter( 'dt_network_dashboard_build_message', 'dt_prayer_list_build_message', 10, 1 );
 function dt_prayer_list_build_message( $activity_log ){
 
-    foreach ( $activity_log as $index => $log ){
+    if ( ! function_exists( 'dt_create_initials' ) ) {
+        foreach ( $activity_log as $index => $log ){
 
-        /* prayer_list_app */
-        if ( 'prayer_list_app' === $log['action'] ) {
-            $initials = Zume_Public_Heatmap_100hours_Utilities::create_initials( $log['lng'], $log['lat'], $log['payload'] );
-            $initials_2 = Zume_Public_Heatmap_100hours_Utilities::create_initials( $log['lng'], $log['lat'], $log['payload'] );
-            $activity_log[$index]['message'] = $initials . ' is praying for ' . $initials_2 . "( location)";
+            /* prayer_list_app */
+            if ( 'prayer_list_app' === $log['action'] ) {
+                $initials = dt_create_initials( $log['lng'], $log['lat'], $log['payload'] );
+                $initials_2 = dt_create_initials( $log['lng'], $log['lat'], $log['payload'] );
+                $activity_log[$index]['message'] = $initials . ' is praying for ' . $initials_2 . "( location)";
+            }
+
         }
-
     }
 
     return $activity_log;
